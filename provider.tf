b4a162da3beacbd2e179614caff286231f616b32
fshuva <fshuva@fortinet.com>
2021-08-26 14:09:13 -0700

    fixing block

--- provider.tf
+++ provider.tf
@@ -5,13 +5,14 @@ terraform {
       source  = "IBM-Cloud/ibm"
       version = "~> 1.20.0"
     }
+  }
+
   backend "remote" {
     organization = "fortinet-ffcdevops"
       workspaces {
         name = "gh-actions-test"
       }
-    }
-  }
+   }
 }
 
 # Configure the IBM Provider
@@ -19,5 +20,4 @@ provider "ibm" {
   region           = var.region
   ibmcloud_api_key = var.ibmcloud_api_key
   generation       = 2
-
 }
